0bf5123822847834afd4ce408d61b1034d15c54c
Ahmed Al-Zagri <USER@Users-MacBook-Pro.local>
2020-12-11 20:14:07 +0300

    push to GitHub

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const messagesRoutes = require("./routes/messages");
 const { loginRequired  ,  ensureCorrectUser }  = require("./middleware/auth");
 const db = require("./models")
 
-const PORT = 8081;
+const PORT = process.env.PORT || 8081;
 
 
 // app config
